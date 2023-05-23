d92700ddb94a75a893035a844fbc157b8bc7c7bd
unknown <Matthew.Romanowicz@Gmail.com>
2023-05-22 21:27:35 -0400

    Made to_bytes actually work

--- src/bin_format.rs
+++ src/bin_format.rs
@@ -117,32 +117,96 @@ fn parse_iint(input: &Vec<char>, iint_format: &IIntFormat) -> Result<i64, String
     Ok(match neg {true => res, false => -res})
 }
 
+enum FloatParseState {
+    Integer,
+    Decimal,
+    ExponentUIntFormat
+}
+
+use core::str::FromStr;
+
+#[macro_export]
+macro_rules! str_res {
+    ( $x:expr ) => {
+        {
+            match $x {
+                Ok(a) => Ok(a),
+                Err(a) => Err(a.to_string())
+            }
+        }
+    };
+}
+
 pub fn to_bytes(input: &Vec<char>, datatype: DataType) -> Result<Vec<u8>, String> {
-    match datatype.fmt {
-        BinaryFormat::UInt(uint_format) => {
-            match parse_uint(input, &uint_format) {
-                Ok(n) => {
-                    match datatype.end {
-                        Endianness::Big => Ok(n.to_be_bytes().to_vec()),
-                        Endianness::Little => Ok(n.to_le_bytes().to_vec()),
-                        Endianness::Network => Ok(n.to_ne_bytes().to_vec())
-                    }
-                },
-                Err(msg) => Err(msg)
+    let s: String = input.iter().collect::<String>();
+    match &datatype.end {
+        Endianness::Big => {
+            match &datatype.fmt {
+                BinaryFormat::UInt(UIntFormat::U8)  => Ok((str_res!(s.parse::<u8>())?).to_be_bytes().to_vec()),
+                BinaryFormat::UInt(UIntFormat::U16) => Ok((str_res!(s.parse::<u16>())?).to_be_bytes().to_vec()),
+                BinaryFormat::UInt(UIntFormat::U32) => Ok((str_res!(s.parse::<u32>())?).to_be_bytes().to_vec()),
+                BinaryFormat::UInt(UIntFormat::U64) => Ok((str_res!(s.parse::<u64>())?).to_be_bytes().to_vec()),
+                BinaryFormat::IInt(IIntFormat::I8)  => Ok((str_res!(s.parse::<i8>())?).to_be_bytes().to_vec()),
+                BinaryFormat::IInt(IIntFormat::I16) => Ok((str_res!(s.parse::<i16>())?).to_be_bytes().to_vec()),
+                BinaryFormat::IInt(IIntFormat::I32) => Ok((str_res!(s.parse::<i32>())?).to_be_bytes().to_vec()),
+                BinaryFormat::IInt(IIntFormat::I64) => Ok((str_res!(s.parse::<i64>())?).to_be_bytes().to_vec()),
+                BinaryFormat::Float(FloatFormat::F32) => Ok((str_res!(s.parse::<f32>())?).to_be_bytes().to_vec()),
+                BinaryFormat::Float(FloatFormat::F64) => Ok((str_res!(s.parse::<f64>())?).to_be_bytes().to_vec())
             }
         },
-        BinaryFormat::IInt(iint_format) => {
-            match parse_iint(input, &iint_format) {
-                Ok(n) => {
-                    match datatype.end {
-                        Endianness::Big => Ok(n.to_be_bytes().to_vec()),
-                        Endianness::Little => Ok(n.to_le_bytes().to_vec()),
-                        Endianness::Network => Ok(n.to_ne_bytes().to_vec())
-                    }
-                },
-                Err(msg) => Err(msg)
+        Endianness::Little => {
+            match &datatype.fmt {
+                BinaryFormat::UInt(UIntFormat::U8)  => Ok((str_res!(s.parse::<u8>())?).to_le_bytes().to_vec()),
+                BinaryFormat::UInt(UIntFormat::U16) => Ok((str_res!(s.parse::<u16>())?).to_le_bytes().to_vec()),
+                BinaryFormat::UInt(UIntFormat::U32) => Ok((str_res!(s.parse::<u32>())?).to_le_bytes().to_vec()),
+                BinaryFormat::UInt(UIntFormat::U64) => Ok((str_res!(s.parse::<u64>())?).to_le_bytes().to_vec()),
+                BinaryFormat::IInt(IIntFormat::I8)  => Ok((str_res!(s.parse::<i8>())?).to_le_bytes().to_vec()),
+                BinaryFormat::IInt(IIntFormat::I16) => Ok((str_res!(s.parse::<i16>())?).to_le_bytes().to_vec()),
+                BinaryFormat::IInt(IIntFormat::I32) => Ok((str_res!(s.parse::<i32>())?).to_le_bytes().to_vec()),
+                BinaryFormat::IInt(IIntFormat::I64) => Ok((str_res!(s.parse::<i64>())?).to_le_bytes().to_vec()),
+                BinaryFormat::Float(FloatFormat::F32) => Ok((str_res!(s.parse::<f32>())?).to_le_bytes().to_vec()),
+                BinaryFormat::Float(FloatFormat::F64) => Ok((str_res!(s.parse::<f64>())?).to_le_bytes().to_vec())
             }
         },
-        _ => todo!()
+        Endianness::Network => {
+            match &datatype.fmt {
+                BinaryFormat::UInt(UIntFormat::U8)  => Ok((str_res!(s.parse::<u8>())?).to_ne_bytes().to_vec()),
+                BinaryFormat::UInt(UIntFormat::U16) => Ok((str_res!(s.parse::<u16>())?).to_ne_bytes().to_vec()),
+                BinaryFormat::UInt(UIntFormat::U32) => Ok((str_res!(s.parse::<u32>())?).to_ne_bytes().to_vec()),
+                BinaryFormat::UInt(UIntFormat::U64) => Ok((str_res!(s.parse::<u64>())?).to_ne_bytes().to_vec()),
+                BinaryFormat::IInt(IIntFormat::I8)  => Ok((str_res!(s.parse::<i8>())?).to_ne_bytes().to_vec()),
+                BinaryFormat::IInt(IIntFormat::I16) => Ok((str_res!(s.parse::<i16>())?).to_ne_bytes().to_vec()),
+                BinaryFormat::IInt(IIntFormat::I32) => Ok((str_res!(s.parse::<i32>())?).to_ne_bytes().to_vec()),
+                BinaryFormat::IInt(IIntFormat::I64) => Ok((str_res!(s.parse::<i64>())?).to_ne_bytes().to_vec()),
+                BinaryFormat::Float(FloatFormat::F32) => Ok((str_res!(s.parse::<f32>())?).to_ne_bytes().to_vec()),
+                BinaryFormat::Float(FloatFormat::F64) => Ok((str_res!(s.parse::<f64>())?).to_ne_bytes().to_vec())
+            }
+        },
     }
 }
+
+// pub fn to_bytes(input: &Vec<char>, datatype: DataType) -> Result<Vec<u8>, String> {
+//     match input.iter().collect::<String>().parse::<i16>() {
+//         Ok(n) => println!("Number: {}", n),
+//         Err(msg) => println!("Error: {}", msg.to_string())
+//     };
+//     match datatype.fmt {
+//         BinaryFormat::UInt(uint_format) => {
+//             match parse_uint(input, &uint_format) {
+//                 Ok(n) => {
+//                     Ok(uint_to_bytes(n, &datatype.end, &uint_format))
+//                 },
+//                 Err(msg) => Err(msg)
+//             }
+//         },
+//         BinaryFormat::IInt(iint_format) => {
+//             match parse_iint(input, &iint_format) {
+//                 Ok(n) => {
+//                     Ok(iint_to_bytes(n, &datatype.end, &iint_format))
+//                 },
+//                 Err(msg) => Err(msg)
+//             }
+//         },
+//         _ => todo!()
+//     }
+// }
